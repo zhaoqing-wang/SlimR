98cc7954b0ed5b88a36de7a01eb2f4bf18f3813a
zhaoqing-wang <851091628@qq.com>
2026-06-03 18:55:00 +0200

    20260603 1850 commit

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,5 @@
 utils::globalVariables(
   c("processed", "Gene", "Counts", "metric", "score", "original",
-    "scaled_score", "gene", "cluster", "mean_expression", "Avg_exp")
+  "scaled_score", "gene", "cluster", "mean_expression", "Avg_exp",
+  "sub_cell_type","main_group","n","prop")
 )
